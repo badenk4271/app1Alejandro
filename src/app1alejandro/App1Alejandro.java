c88ab00e7ac2ea5da189fb5727e665fb9a1455f6
badenk4271 <ayavarav@gmail.com>
2017-10-25 21:01:46 -0300

    aca se realizo la operacion matematica del ejercicio 3

---
+++ src/app1alejandro/App1Alejandro.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app1alejandro;
+
+/**
+ *
+ * @author LAB01
+ */
+public class App1Alejandro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        int numero1;
+        int numero2;
+        
+        numero1=120;
+        numero2=30;
+        
+        System.out.println("suma de numero1 y numero2:"+(numero1+numero2));
+        System.out.println("resta de numero1 y numero2:"+(numero1-numero2));
+        System.out.println("multiplicar de numero1 y numero2:"+(numero1*numero2));
+        System.out.println("dividir de numero1 y numero2:"+(numero1/numero2));
+        System.out.println("reciduo de numero1 y numero2:"+(numero1&numero2));
+    }
+    
+}
